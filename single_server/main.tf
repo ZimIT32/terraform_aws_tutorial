86cf282751fab796e464ee10951952fbab7c29f4
Marco Spretke <marco.spretke@5ca.com>
2020-03-28 21:27:46 +0100

    porudction ready

--- single_server/main.tf
+++ single_server/main.tf
@@ -2,6 +2,13 @@ provider "aws" {
     region = "us-east-2"
 }
 
+variable "http_port" {
+    description = "The HTTP port we want to use"
+
+    type    = number
+    default = 8080
+}
+
 resource "aws_instance" "example" {
     ami                     = "ami-0c55b159cbfafe1f0"
     instance_type           = "t2.micro"
@@ -10,7 +17,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World!" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.http_port} &
                 EOF
 
     tags = {
@@ -22,9 +29,14 @@ resource "aws_security_group" "instance" {
     name ="terraform-example-instance"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.http_port
+        to_port     = var.http_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
 }
